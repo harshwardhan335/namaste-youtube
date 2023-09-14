55592fa81b81598c64da9800ee2eb91f8e684848
harshwardhan335 <wardhan335@gmail.com>
2023-09-14 14:02:53 +0530

    chapter-16 machine coding round

---
+++ src/components/ButtonList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from './Button'
+
+const ButtonList = () => {
+  return (
+    <div className='flex'>
+      <Button name="All"/>
+      <Button name="Song"/>
+      <Button name="Live"/>
+      <Button name="Soccer"/>
+      <Button name="Cricket"/>
+      <Button name="Cooking"/>
+      <Button name="News"/>
+      <Button name="Gaming"/>
+      <Button name="Gaming"/>
+    </div>
+  )
+}
+
+export default ButtonList
