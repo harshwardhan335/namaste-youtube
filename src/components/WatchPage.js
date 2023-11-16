469e30220494872bb387ad4999f8a7546d81921f
harshwardhan335 <wardhan335@gmail.com>
2023-11-16 16:41:03 +0530

    Youtube advance (Debouncing, nested comment)

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -12,6 +13,7 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
   return (
+    <div className="flex flex-col">
     <div className="px-5">
       <iframe
         width="1000"
@@ -22,7 +24,10 @@ const WatchPage = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen></iframe>
     </div>
+    <CommentsContainer/>
+    </div>
   );
 };
+
 
 export default WatchPage;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,6 @@ const GOOGLE_API_KEY = "AIzaSyCW-53eUyP8-UZBu-3QPS5cJt0_osXZ7do";
 
 export const YOUTUBE_VIDEO_API = 
 "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
